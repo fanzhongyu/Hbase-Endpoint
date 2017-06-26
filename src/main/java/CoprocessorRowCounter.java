88618c2db1f7ff20923c5347c20c6c8e4e2e40fa
fanzhongyu <fanzhongyu@tongfangcloud.com>
2017-06-26 23:21:16 +0800

    实现了简单的用Endpoint协处理器实现RowCount

--- src/main/java/CoprocessorRowCounter.java
+++ src/main/java/CoprocessorRowCounter.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.client.coprocessor.Batch;
+import org.apache.hadoop.hbase.coprocessor.example.generated.ExampleProtos;
+import org.apache.hadoop.hbase.ipc.BlockingRpcCallback;
 import org.apache.hadoop.hbase.ipc.ServerRpcController;
 
 import java.io.IOException;
@@ -14,27 +16,40 @@ import java.util.Map;
  * Created by fanzhongyu on 2017/6/22.
  */
 public class CoprocessorRowCounter {
-    public static void main() throws IOException {
+    public static void main(String[] args) throws Throwable {
         Configuration conf = HBaseConfiguration.create();
+        conf.set("hbase.rootdir","hdfs://valukacluster/hbase");
+        conf.set("hbase.zookeeper.quorum","zk-1,zk-2,zk-3");
+        conf.set("hbase.zookeeper.property.clientPort","30000");
         Connection hCon;
         hCon = ConnectionFactory.createConnection(conf);
         Table table = hCon.getTable(TableName.valueOf("student"));
 
-//        final ExampleProtos.CountRequest request = ExampleProtos.CountRequest.getDefaultInstance();
-//        Map<byte[],Long> results = table.coprocessorService(ExampleProtos.RowCountService.class,
-//                null, null,
-//                new Batch.Call<ExampleProtos.RowCountService,Long>() {
-//                    public Long call(ExampleProtos.RowCountService counter) throws IOException {
-//                        ServerRpcController controller = new ServerRpcController();
-//                        BlockingRpcCallback<ExampleProtos.CountResponse> rpcCallback =
-//                                new BlockingRpcCallback<ExampleProtos.CountResponse>();
-//                        counter.getRowCount(controller, request, rpcCallback);
-//                        ExampleProtos.CountResponse response = rpcCallback.get();
-//                        if (controller.failedOnException()) {
-//                            throw controller.getFailedOn();
-//                        }
-//                        return (response != null && response.hasCount()) ? response.getCount() : 0;
-//                    }
-//                });
+        final ExampleProtos.CountRequest request = ExampleProtos.CountRequest.getDefaultInstance();
+        Map<byte[],Long> results = table.coprocessorService(ExampleProtos.RowCountService.class,
+                null, null,
+                new Batch.Call<ExampleProtos.RowCountService,Long>() {
+                    public Long call(ExampleProtos.RowCountService counter) throws IOException {
+                        ServerRpcController controller = new ServerRpcController();
+                        BlockingRpcCallback<ExampleProtos.CountResponse> rpcCallback =
+                                new BlockingRpcCallback<ExampleProtos.CountResponse>();
+                        counter.getRowCount(controller, request, rpcCallback);
+                        ExampleProtos.CountResponse response = rpcCallback.get();
+                        if (controller.failedOnException()) {
+                            throw controller.getFailedOn();
+                        }
+                        return (response != null && response.hasCount()) ? response.getCount() : 0;
+                    }
+                });
+        long sum = 0;
+        int count = 0;
+        for(Long l : results.values()){
+            sum += l;
+            count ++;
+        }
+
+        System.out.println("Region count " + count);
+        System.out.println("Rowkey count " + sum);
+
     }
 }
